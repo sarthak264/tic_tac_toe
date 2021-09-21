bc678a385c5a1935f0d3e07bac7426a755e47ad1
sarthak264 <sarthaksharm372@gmail.com>
2021-09-21 15:54:24 +0530

    ui is completed and winning scenarios list is made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
+import "./app.css";
+
 function App() {
   return (
     <>
-      <h1>Todos List</h1>
-    </>    
+      <div id="game">
+        <h1>Tic Tac Toe</h1>
+        <div id="board">
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+        </div>
+      </div>
+
+      <div id="modal-overlay">
+        <div id="game-result-modal">
+          <div id="result-container">
+            <div id="winner-container">
+              <span></span>
+            </div>
+          </div>
+          <div id="new-game-container">
+            <button id="new-game-button">Start New Game</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
