3e3fa541482611d06c7d6fb1c0425ace9bc6e81c
sarthak264 <sarthaksharm372@gmail.com>
2021-09-21 23:40:37 +0530

    fixed cell overwriting issue

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,24 +1,45 @@
 import { useState } from "react";
 import "./game.css";
-import Board from './../board/Board'
+import Board from "./../board/Board";
 
 const Game = () => {
-  const [cellValues,setCellValues] = useState(['','','','','','','','','']);
-  const [mark,setMark] = useState(true)
+  // useStates
+  const [cellValues, setCellValues] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  const [mark, setMark] = useState(true);
+
+  // Misc variables
   const winningCombination = [];
+
+  // Functions
+  const isCellEmpty = (cellIndex) => cellValues[cellIndex] === "";
   const cellClicked = (cellIndex) => {
-    const newCellValues = [...cellValues];
-    newCellValues[cellIndex] = mark?'X':'O';
-    setMark(!mark);
-    setCellValues(newCellValues);
-    console.log(`cell ${cellIndex} clicked.`);
-  }
+    if (isCellEmpty(cellIndex)) {
+      const newCellValues = [...cellValues];
+      newCellValues[cellIndex] = mark ? "X" : "O";
+      setMark(!mark);
+      setCellValues(newCellValues);
+    }
+  };
 
   return (
     <>
       <div id="game">
         <h1>Tic Tac Toe</h1>
-        <Board cellValues={cellValues} winningCombination={winningCombination} cellClicked={cellClicked}/>
+        <Board
+          cellValues={cellValues}
+          winningCombination={winningCombination}
+          cellClicked={cellClicked}
+        />
       </div>
 
       <div id="modal-overlay">
