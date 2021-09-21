805a21a631c21bb19b4ad0c55cebec3752862a71
sarthak264 <sarthaksharm372@gmail.com>
2021-09-21 22:39:02 +0530

    winningCombination can be set dynamically now

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -3,11 +3,15 @@ import Cell from "./../cell/Cell";
 
 const Board = () => {
   const cellValues = ['X','X','X','X','O','X','O','',''];
+  const winningCombination = [0,4,8];
+
   return (
     <div id="board">
       {cellValues.map((item, index) => {
+        const highlight = winningCombination && 
+                          winningCombination.indexOf(index) >= 0;
         return (
-          <Cell key={index} value={item} highlight={false} />
+          <Cell key={index} value={item} highlight={highlight} />
         )
       })}
     </div>
